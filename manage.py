1f2f4d5273d7fb3dd7401c0c301a47ca5f4cfa2a
Mubin Attar <sk.mubinattar@gmail.com>
2024-03-14 11:30:01 +0530

    Update manage.py
    Improve error handling, documentation, and command-line argument parsing

--- manage.py
+++ manage.py
@@ -2,21 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import argparse
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snowflake.settings")
+    parser = argparse.ArgumentParser(description='Django management utility')
+    parser.add_argument('--settings', help='Path to the Django settings file')
+    parser.add_argument('--command', help='The name of the Django management command')
+    args = parser.parse_args()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", args.settings)
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line([sys.argv[0], args.command])
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
